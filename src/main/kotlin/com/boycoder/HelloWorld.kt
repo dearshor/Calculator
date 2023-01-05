619dacb5b0af068b008b6c02f6383468346657d8
Dearshor Xue <dearshor@gmail.com>
2023-01-05 17:06:01 +0800

    first version of Calculator

--- src/main/kotlin/com/boycoder/HelloWorld.kt
+++ src/main/kotlin/com/boycoder/HelloWorld.kt
@@ -1,5 +1,58 @@
 package com.boycoder
 
+import kotlin.system.exitProcess
+
+val helpInfo = """
+    ---------------------------------------
+    usage:
+    1. eg. input 1 + 1, press ENTER;
+    2. CAUTION: space is required between numbers and operator;
+    3. to exit, type exit, press ENTER.
+    ---------------------------------------
+""".trimIndent()
 fun main() {
-    println("Hello world.")
+    while (true) {
+        println(helpInfo)
+
+        val input = readlnOrNull() ?: continue
+        if (input == "exit") exitProcess(0)
+
+        val inputList = input.split(" ")
+        val result = caculate(inputList)
+
+        if (result == null) {
+            println("incorrect input")
+            continue
+        } else {
+            println("$input = $result")
+        }
+    }
+}
+
+private fun caculate(inputList: List<String>): Int? {
+    if (inputList.size != 3) return null
+
+    val left = inputList[0].toInt()
+//    val operator = Operation.valueOf(inputList[1])
+    val operator = when (inputList[1]) {
+        "+" -> Operation.ADD
+        "-" -> Operation.MINUS
+        "*" -> Operation.MULTIPLY
+        "/" -> Operation.DIVIDE
+        else -> return null
+    }
+    val right = inputList[2].toInt()
+    return when (operator) {
+        Operation.ADD -> left + right
+        Operation.MINUS -> left - right
+        Operation.MULTIPLY -> left * right
+        Operation.DIVIDE -> left / right
+    }
+}
+
+enum class Operation(val value: String) {
+    ADD("+"),
+    MINUS("-"),
+    MULTIPLY("*"),
+    DIVIDE("/")
 }
